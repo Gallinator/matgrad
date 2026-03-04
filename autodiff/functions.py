7845637764f981a1e512b7ac200d3771d6faad4a
Gallinator <lcgallinator4@gmail.com>
2026-03-04 14:51:25 +0100

    Add functions implementations

---
+++ autodiff/functions.py
@@ -0,0 +1,52 @@
+import sys
+
+import numpy as np
+from autodiff.grad_functions import Reshape, Concatenate, Stack, Sin, Sigmoid, Log, Sum, ReLU, Conv2d, _conv2d_f, Exp
+from autodiff.variable import Variable, any_requires_grad
+
+
+def reshape(v: Variable, shape):
+    return Variable(np.reshape(v.value, shape), Reshape(v), v.requires_grad)
+
+
+def cat(values, dim):
+    return Variable(np.concatenate([v.value for v in values], axis=dim), Concatenate(dim, *values),
+                    any_requires_grad(*values))
+
+
+def stack(values, dim):
+    return Variable(np.stack([v.value for v in values], axis=dim), Stack(dim, *values),
+                    any_requires_grad(*values))
+
+
+def sin(v: Variable):
+    return Variable(np.sin(v.value), Sin(v), v.requires_grad)
+
+
+def sigmoid(v: Variable):
+    return Variable(1 / (1 + np.exp(-v.value)), Sigmoid(v), v.requires_grad)
+
+
+def log(v: Variable):
+    return Variable(np.log(v.value + sys.float_info.epsilon), Log(v), v.requires_grad)
+
+
+def sum(v: Variable, dim=None, keepdims=False):
+    return Variable(np.sum(v.value, axis=dim, keepdims=keepdims), Sum(v, dim, keepdims), v.requires_grad)
+
+
+def relu(v: Variable):
+    return Variable(np.array(np.maximum(0.0, v.value)), ReLU(v), v.requires_grad)
+
+
+def exp(v: Variable):
+    return Variable(np.exp(v.value), Exp(v), v.requires_grad)
+
+
+def softmax(v: Variable, dim):
+    e = exp(v)
+    return e / sum(e, dim, keepdims=True)
+
+
+def conv2d(v: Variable, k: Variable):
+    return Variable(_conv2d_f(v.value, k.value), Conv2d(v, k), any_requires_grad(v, k))
